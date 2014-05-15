29ff793f0b124f6b37851cb27c99c7769dc1a423
Dito <dhartoto@yahoo.com>
2014-05-15 15:35:45 +1000

    Updated Models and Views wit forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,49 @@
 class PostsController < ApplicationController
+
+  before_action :set_post [:show, :edit, :update]
+
+
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
-
+    @post = Post.new
   end
 
   def create
-
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "Your post was created."
+      redirect_to posts_path
+    else
+      @post.errors
+      render :new  
+    end
   end
 
   def edit
-
   end
 
   def update
-
+    if @post.update(post_params) # Why is this not @post.update = post_params
+      flash[:notice] = "You post has been updated!"
+      redirect_to post_path(@post)
+    else
+      @post.errors
+      render edit
+    end
   end
 
+private
+  def post_params
+    params.require(:post).permit!
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_many :post_categories
   has_many :categories, through: :post_categories
+
+  validates :title, presence: true
 end
